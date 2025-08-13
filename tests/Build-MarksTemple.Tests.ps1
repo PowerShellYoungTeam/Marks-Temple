3a6e3161c2858f58e4fb5adcced4e9170d27776f
Wighty <53131013+PowerShellYoungTeam@users.noreply.github.com>
2025-08-13 22:26:31 +0100

    Enhance Pester test output by adding verbose flag and ensure proper path usage for temple file

--- tests/Build-MarksTemple.Tests.ps1
+++ tests/Build-MarksTemple.Tests.ps1
@@ -9,23 +9,23 @@ else {
     $templePath = Resolve-Path "$PSScriptRoot/../src/Temple.txt"
 }
 
-. $scriptPath
+. $scriptPath.Path
 
-if (-not (Test-Path $templePath)) {
-    Write-Warning "Temple.txt not found at $templePath. Skipping Build-MarksTemple tests."
+if (-not (Test-Path $templePath.Path)) {
+    Write-Warning "Temple.txt not found at $($templePath.Path). Skipping Build-MarksTemple tests."
     return
 }
 
 Describe "Build-MarksTemple" {
     It "Should run without error using default parameters" {
-        { Build-MarksTemple -TemplePath $templePath } | Should -Not -Throw
+        { Build-MarksTemple -TemplePath $templePath.Path } | Should -Not -Throw
     }
 
     It "Should run with custom colours" {
-        { Build-MarksTemple -ForeGroundColour 'Red' -BackGroundColour 'White' -TemplePath $templePath } | Should -Not -Throw
+        { Build-MarksTemple -ForeGroundColour 'Red' -BackGroundColour 'White' -TemplePath $templePath.Path } | Should -Not -Throw
     }
 
     It "Should throw with invalid colour" {
-        { Build-MarksTemple -ForeGroundColour 'Orange' -TemplePath $templePath } | Should -Throw
+        { Build-MarksTemple -ForeGroundColour 'Orange' -TemplePath $templePath.Path } | Should -Throw
     }
 }
